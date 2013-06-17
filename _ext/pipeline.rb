83235680183e894b7acce20ac482941ee4b58c81
Jason Brooks <jbrooks@redhat.com>
2013-06-17 08:28:04 +0700

    Update pipeline.rb

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,7 @@
 require 'bootstrap-sass'
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new('/archive')
+  extension Awestruct::Extensions::Posts.new('/archive', :posts)
   extension Awestruct::Extensions::Paginator.new(:posts, '/index', :per_page => 5)
   extension Awestruct::Extensions::Indexifier.new  
 # Indexifier *must* come before Atomizer

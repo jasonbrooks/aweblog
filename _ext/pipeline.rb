0ea926cfed69f09110eda1e3267abd1940781938
Jason Brooks <jbrooks@redhat.com>
2013-06-17 07:34:26 +0700

    tweak to pipeline syntax

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,9 +1,9 @@
 require 'bootstrap-sass'
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new '/archive'
-  extension Awestruct::Extensions::Indexifier.new
-  extension Awestruct::Extensions::Paginator.new(:posts, '/index', :per_page => 2)
-  # Indexifier *must* come before Atomizer
+  extension Awestruct::Extensions::Posts.new('/archive')
+  extension Awestruct::Extensions::Paginator.new(:posts, '/index', :per_page => 5)
+  extension Awestruct::Extensions::Indexifier.new  
+# Indexifier *must* come before Atomizer
   extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
 end
